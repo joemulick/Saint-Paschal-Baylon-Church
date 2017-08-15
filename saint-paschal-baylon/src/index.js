7aee625fe7ad7ac4b47a1e65077658c6fccb8bbc
Joe Mulick <joemulick@gmail.com>
2017-08-15 12:09:55 -0700

    In order to achieve maximum browser compatibility we have decided not to use react which does not support browsers lower than IE9. Secondly react does not benefit from css preprocessors. The new website will implement a Babel transpiler and will run on ES6 with the use of a css preprocessor.

--- saint-paschal-baylon/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './stylesheets/base/index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import 'bootstrap/dist/css/bootstrap.css';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
