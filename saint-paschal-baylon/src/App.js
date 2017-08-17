da98e9da041c25f9a2c8bebda489869d8c5ed188
Joe Mulick <joemulick@gmail.com>
2017-08-17 13:25:14 -0700

    Navbar loaded properly but has not been customized or edited yet

--- saint-paschal-baylon/src/App.js
+++ saint-paschal-baylon/src/App.js
@@ -3,17 +3,48 @@ import './stylesheets/base/App.css';
 // import Navbar from './components/static/Navbar.js';
 // import Footer from './components/static/Footer.js';
 import Main from './components/Main.js';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+
 
 class App extends Component {
-  render() {
-    return (
-      <div>
+	render() {
+		return (
+			<div>
+				<Navbar collapseOnSelect>
+					<Navbar.Header>
+						<Navbar.Brand>
+						    <a href="#">Saint Paschal Baylon</a>
+						</Navbar.Brand>
 
-          <Main />
+						<Navbar.Toggle />
+					</Navbar.Header>
+						<Navbar.Collapse>
+						  <Nav>
+						    <NavItem eventKey={1} href="#">Link</NavItem>
+						    <NavItem eventKey={2} href="#">Link</NavItem>
 
-      </div>
-    );
-  }
+						    <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+						      <MenuItem eventKey={3.1}>Action</MenuItem>
+						      <MenuItem eventKey={3.2}>Another action</MenuItem>
+						      <MenuItem eventKey={3.3}>Something else here</MenuItem>
+						      <MenuItem divider />
+						      <MenuItem eventKey={3.3}>Separated link</MenuItem>
+						    </NavDropdown>
+
+						  </Nav>
+						  <Nav pullRight>
+						    <NavItem eventKey={1} href="#">Link Right</NavItem>
+						    <NavItem eventKey={2} href="#">Link Right</NavItem>
+						  </Nav>
+					</Navbar.Collapse>
+				</Navbar>
+
+				<Main />
+
+			</div>
+		);
+	}
+
 }
 
 export default App;
