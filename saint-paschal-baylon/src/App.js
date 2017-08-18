48f5a1ce0b320d8e49fd506defd83164e55fb0eb
Joe Mulick <joemulick@gmail.com>
2017-08-17 20:59:02 -0700

    Navbar displaying correctly and homepage row and column added successfully.

--- saint-paschal-baylon/src/App.js
+++ saint-paschal-baylon/src/App.js
@@ -4,19 +4,14 @@ import Home from './components/Home.js';
 import BecomeCatholic from './components/BecomeCatholic.js';
 import GetInvolved from './components/GetInvolved.js';
 import ContactUs from './components/ContactUs.js';
+import { LinkContainer } from 'react-router-bootstrap';
 import {
   BrowserRouter as Router,
   Route,
-  Link
 } from 'react-router-dom';
-import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem, Grid } from 'react-bootstrap';
 
 /* Styling */
-
-let noStyle = {
-  textDecoration: 'none',
-};
-
 
 
 /* Styling End */
@@ -26,40 +21,57 @@ class App extends Component {
 		return (
 			<Router>
 				<div>
-
 					<Navbar collapseOnSelect>
 						<Navbar.Header>
+
 							<Navbar.Brand>
-							    <Link to = "/">Saint Paschal Baylon</Link>
+								<LinkContainer to="/">
+							    	<a>Saint Paschal Baylon</a>
+							    </LinkContainer>
 							</Navbar.Brand>
 
 							<Navbar.Toggle />
+
 						</Navbar.Header>
 							<Navbar.Collapse>
 							  <Nav>
-							    <NavItem eventKey={1}><Link to="/becoming-catholic" style={noStyle}>Becoming Catholic</Link></NavItem>
-							    <NavItem eventKey={2}>Mass Times</NavItem>
-							    <NavItem eventKey={3}>Upcoming Events</NavItem>
+							  	<LinkContainer to="/becoming-catholic">
+							    	<NavItem eventKey={1}>Becoming Catholic</NavItem>
+							    </LinkContainer>
+
+							    <LinkContainer to="#">
+							    	<NavItem eventKey={2}>Mass Times</NavItem>
+							    </LinkContainer>
+
+							    <LinkContainer to="#">
+							    	<NavItem eventKey={3}>Upcoming Events</NavItem>
+							    </LinkContainer>
 
 							    <NavDropdown eventKey={3} title="Church Amenities" id="basic-nav-dropdown">
 							      <MenuItem eventKey={3.1}>Adoration Chapel</MenuItem>
 							      <MenuItem eventKey={3.2}>Confession</MenuItem>
 							      <MenuItem eventKey={3.3}>Crying Room</MenuItem>
 							    </NavDropdown>
-
 							  </Nav>
+
 							  <Nav pullRight>
-							  	<NavItem eventKey={1}><Link to="/get-involved">Get Involved</Link></NavItem>
-							    <NavItem eventKey={2}><Link to="/contact-us">Contact Us</Link></NavItem>
+							  	<LinkContainer to="/get-involved">
+							  		<NavItem eventKey={1}>Get Involved</NavItem>
+							  	</LinkContainer>
+							  	<LinkContainer to="/contact-us">
+							    	<NavItem eventKey={2}>Contact Us</NavItem>
+							    </LinkContainer>
 							  </Nav>
 						</Navbar.Collapse>
 					</Navbar>
+				<Grid>
 
 				<Route exact path="/" component={Home}/>
 		        <Route path="/becoming-catholic" component={BecomeCatholic}/>
 		        <Route path="/get-involved" component={GetInvolved}/>
 		        <Route path="/contact-us" component={ContactUs}/>
 
+		        </Grid>
 				</div>
 			</Router>
 		);
--- saint-paschal-baylon/src/components/Home.js
+++ saint-paschal-baylon/src/components/Home.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
+import { LinkContainer } from 'react-router-bootstrap';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
+import { Row, Col } from 'react-bootstrap';
 
 
 
 class Home extends Component {
   render() {
-    return (
-      <div>
+    return (    	
+    	<div>
 
-      <p>Home Page Content</p>
+    	<Row className="show-grid">
+	    	<Col><p>HomePage</p></Col>
+    	</Row>
 
-      </div>
+    	</div>
     );
   }
 }
